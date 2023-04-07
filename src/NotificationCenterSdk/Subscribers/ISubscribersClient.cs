46896fc40db2d9f1a2f8401c9120d2dc1ea4d170
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:21:15 +0200

    ISubscribersClient.cs interface added

---
+++ src/NotificationCenterSdk/Subscribers/ISubscribersClient.cs
@@ -0,0 +1,8 @@
+﻿namespace NotificationCenterSdk.Subscribers;
+
+public interface ISubscribersClient
+{
+    Task<CreateSubscriberResult> CreateSubscriberAsync(Subscriber subscriber);
+    Task<IEnumerable<Notification>> GetSubscriberNotificationAsync(string subscriberId, int pageIndex, int pageLimit);
+    Task MarkNotificationsAsReadAsync(string subscriberId, IEnumerable<string> notificationsIds);
+}
