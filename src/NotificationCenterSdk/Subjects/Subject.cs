29feb9f4a33cd9cff21b2fb080454ad6901279ab
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:15:50 +0200

    Subject.cs model added

---
+++ src/NotificationCenterSdk/Subjects/Subject.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NotificationCenterSdk.Subjects;
+
+public class Subject
+{
+    [JsonPropertyName("_id")]
+    public string Id { get; set; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    
+    [JsonPropertyName("realm")]
+    public string realmId { get; set; }
+    
+    [JsonPropertyName("createdAt")]
+    public DateTime CreatedAt { get; set; }
+    
+    [JsonPropertyName("updatedAt")]
+    public DateTime UpdatedAt { get; set; }
+}
