d18b5f4b0a32927b2403a2e8aa945d7a091e7ce0
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:10:51 +0200

    notification center credentials added

---
+++ src/NotificationCenterSdk/NotificationCenterCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NotificationCenterSdk;
+
+public class NotificationCenterCredentials
+{
+    public NotificationCenterCredentials(string apiSecret)
+    {
+        apiSecret = apiSecret;
+    }
+    
+    [JsonPropertyName("apiSecret")]
+    public string ApiSecret { get; }
+}
