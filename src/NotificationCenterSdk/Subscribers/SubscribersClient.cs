f575d987584062b220a04a8242055d5f4aea646d
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:21:47 +0200

    SubscribersClient.cs implemented

---
+++ src/NotificationCenterSdk/Subscribers/SubscribersClient.cs
@@ -0,0 +1,24 @@
+﻿namespace NotificationCenterSdk.Subscribers;
+
+public class SubscribersClient : BaseClient, ISubscribersClient
+{
+    public SubscribersClient(IApiClient apiClient)
+        : base(apiClient, Constants.SUBSCRIBERS_PATH)
+    {
+    }
+
+    public Task<CreateSubscriberResult> CreateSubscriberAsync(Subscriber subscriber)
+    {
+        return ApiClient.PostAsync<CreateSubscriberResult>(Path, subscriber);
+    }
+
+    public Task<IEnumerable<Notification>> GetSubscriberNotificationAsync(string subscriberId, int pageIndex, int pageLimit)
+    {
+        return ApiClient.GetAsync<IEnumerable<Notification>>($"{Path}/{subscriberId}/notifications");
+    }
+
+    public Task MarkNotificationsAsReadAsync(string subscriberId, IEnumerable<string> notificationsIds)
+    {
+        return ApiClient.PatchAsync<object>($"{Path}/{subscriberId}/marksomeasread", notificationsIds);
+    }
+}
