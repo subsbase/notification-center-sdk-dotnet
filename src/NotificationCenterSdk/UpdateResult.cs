2ddfc94e6a8d0e56087b9dbf1def5b16c188222a
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:07:23 +0200

    update result added

---
+++ src/NotificationCenterSdk/UpdateResult.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NotificationCenterSdk;
+
+public class UpdateResult
+{
+    [JsonPropertyName("matchedCount")]
+    public int MatchedCount { get; set; }
+    
+    [JsonPropertyName("modifiedCount")]
+    public int ModifiedCount { get; set; }
+    
+    [JsonPropertyName("acknowledged")]
+    public bool Acknowledged { get; set; }
+    
+    [JsonPropertyName("upsertedId")]
+    public IEnumerable<string> UpsertedIds { get; set; }
+    
+    [JsonPropertyName("upsertedCount")]
+    public int UpsertedCount { get; set; }
+}
