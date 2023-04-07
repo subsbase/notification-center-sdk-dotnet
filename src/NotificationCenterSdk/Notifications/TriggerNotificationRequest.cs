6f63d5f61fe6a0d4d818b90250bae8a153635448
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:25:44 +0200

    TriggerNotificationRequest.cs model added

---
+++ src/NotificationCenterSdk/Notifications/TriggerNotificationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NotificationCenterSdk.Notifications;
+
+public class TriggerNotificationRequest
+{
+    [JsonPropertyName("to")]
+    public List<string> To { get; set; } = new();
+    
+    [JsonPropertyName("payload")]
+    public object Payload { get; set; }
+    
+    [JsonPropertyName("actionUrl")]
+    public string ActionUrl { get; set; }
+}
