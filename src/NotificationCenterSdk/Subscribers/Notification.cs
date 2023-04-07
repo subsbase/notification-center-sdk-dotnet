e5e724cf2f669c3634e8b3ad3c1637fb4bd32820
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:19:09 +0200

    Subscriber Notification.cs model added

---
+++ src/NotificationCenterSdk/Subscribers/Notification.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NotificationCenterSdk.Subscribers;
+
+public class Notification
+{
+    [JsonPropertyName("_id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("topic")]
+    public string TopicId { get; set; }
+    
+    [JsonPropertyName("content")]
+    public string Content { get; set; }
+    
+    [JsonPropertyName("actionUrl")]
+    public string ActionUrl { get; set; }
+    
+    [JsonPropertyName("read")]
+    public bool Read { get; set; }
+    
+    [JsonPropertyName("createdAt")]
+    public DateTime CreatedAt { get; set; }
+    
+    [JsonPropertyName("updatedAt")]
+    public DateTime UpdatedAt { get; set; }
+}
