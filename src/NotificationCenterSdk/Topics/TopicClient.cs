8fcc058016533cbc2c64f9964c2d42d253c333a0
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:25:18 +0200

    TopicClient.cs implemented

---
+++ src/NotificationCenterSdk/Topics/TopicClient.cs
@@ -0,0 +1,16 @@
+﻿namespace NotificationCenterSdk.Topics;
+
+public class TopicClient : BaseClient, ITopicsClient
+{
+    private readonly IApiClient _apiClient;
+
+    public TopicClient(IApiClient apiClient)
+        : base(apiClient, Constants.TOPICS_PATH)
+    {
+    }
+
+    public Task<CreateTopicResult> CreateTopicAsync(Topic topic)
+    {
+        return ApiClient.PostAsync<CreateTopicResult>(Path, topic);
+    }
+}
