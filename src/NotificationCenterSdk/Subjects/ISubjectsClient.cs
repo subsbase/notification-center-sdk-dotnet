5c461c6d43322ca714d5c2db7b87b68a369f786d
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:17:10 +0200

    ISubjectsClient.cs interface added

---
+++ src/NotificationCenterSdk/Subjects/ISubjectsClient.cs
@@ -0,0 +1,9 @@
+﻿namespace NotificationCenterSdk.Subjects;
+
+public interface ISubjectsClient
+{
+    Task<IEnumerable<Subject>> ListSubjectsAsync(int pageIndex, int pageLimit);
+    Task<Subject> GetSubjectByIdAsync(string subjectId);
+    Task<CreateSubjectResult> CreateSubjectAsync(Subject subject);
+    Task<UpdateSubjectResult> UpdateSubjectAsync(Subject subject);
+}
