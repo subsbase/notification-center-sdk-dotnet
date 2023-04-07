d73ffde3d1951d9bd51a594080bcf833f5697400
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:27:14 +0200

    NotificationsClient.cs implemented

---
+++ src/NotificationCenterSdk/Notifications/NotificationsClient.cs
@@ -0,0 +1,14 @@
+﻿namespace NotificationCenterSdk.Notifications;
+
+public class NotificationsClient : BaseClient, INotificationsClient
+{
+    public NotificationsClient(IApiClient apiClient)
+        : base(apiClient, Constants.NOTIFICATIONS_PATH)
+    {
+    }
+
+    public Task TriggerAsync(string topicEvent, TriggerNotificationRequest request)
+    {
+        return ApiClient.PostAsync<object>($"{Path}/trigger/{topicEvent}", request);
+    }
+}
