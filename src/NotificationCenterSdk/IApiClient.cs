704fdb9857585d4d65f847ded8e2efdc29e0659a
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:09:14 +0200

    Api Client added

---
+++ src/NotificationCenterSdk/IApiClient.cs
@@ -0,0 +1,10 @@
+﻿namespace NotificationCenterSdk;
+
+public interface IApiClient
+{
+    Task<TResponse> GetAsync<TResponse>(string path, IDictionary<string, string>? headers = null);
+    Task<TResponse> PostAsync<TResponse>(string path, object body, TResponse response, IDictionary<string, string>? headers = null);
+    Task<TResponse> PostAsync<TResponse>(string path, object body, IDictionary<string, string>? headers = null);
+    Task<TResponse> PutAsync<TResponse>(string path, object body, IDictionary<string, string>? headers = null);
+    Task<TResponse> PatchAsync<TResponse>(string path, object body, IDictionary<string, string>? headers = null);
+}
