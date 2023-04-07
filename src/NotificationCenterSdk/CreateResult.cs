1dd79a10cd5a78b97c2b416ad30ca0974e4b5dd5
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:07:43 +0200

    create result added

---
+++ src/NotificationCenterSdk/CreateResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NotificationCenterSdk;
+
+public class CreateResult
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("created")]
+    public bool Created { get; set; }
+}
