78780e8eb23228f380236ca711ff6b641580dd4b
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:17:49 +0200

    SubjectsClient.cs implemented

---
+++ src/NotificationCenterSdk/Subjects/SubjectsClient.cs
@@ -0,0 +1,29 @@
+﻿namespace NotificationCenterSdk.Subjects;
+
+public class SubjectsClient : BaseClient, ISubjectsClient
+{
+    public SubjectsClient(IApiClient apiClient)
+        :base(apiClient,Constants.SUBJECTS_PATH)
+    {
+    }
+
+    public Task<IEnumerable<Subject>> ListSubjectsAsync(int pageIndex, int pageLimit)
+    {
+        return ApiClient.GetAsync<IEnumerable<Subject>>(Path);
+    }
+
+    public Task<Subject> GetSubjectByIdAsync(string subjectId)
+    {
+        return ApiClient.GetAsync<Subject>($"{Path}/{subjectId}");
+    }
+    
+    public Task<CreateSubjectResult> CreateSubjectAsync(Subject subject)
+    {
+        return ApiClient.PostAsync<CreateSubjectResult>(Path, subject);
+    }
+
+    public Task<UpdateSubjectResult> UpdateSubjectAsync(Subject subject)
+    {
+        return ApiClient.PutAsync<UpdateSubjectResult>(Path, subject);
+    }
+}
