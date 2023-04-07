d67d219f5e5dc257ca43c4006ea7738c87dd7b55
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:28:04 +0200

    INotificationCenterApi.cs added

---
+++ src/NotificationCenterSdk/INotificationCenterApi.cs
@@ -0,0 +1,17 @@
+﻿using NotificationCenterSdk.Notifications;
+using NotificationCenterSdk.Subjects;
+using NotificationCenterSdk.Subscribers;
+using NotificationCenterSdk.Topics;
+
+namespace NotificationCenterSdk;
+
+public interface INotificationCenterApi
+{
+    INotificationsClient NotificationsClient();
+
+    ISubscribersClient SubscribersClient();
+
+    ISubjectsClient SubjectsClient();
+    
+    ITopicsClient TopicsClient();
+}
