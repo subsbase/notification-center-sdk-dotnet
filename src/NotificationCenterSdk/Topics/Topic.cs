4f43944e3e0d38126f6b1919d84a96ca3333908c
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:24:01 +0200

    Topic.cs model added

---
+++ src/NotificationCenterSdk/Topics/Topic.cs
@@ -0,0 +1,31 @@
+﻿using NotificationCenterSdk.Subscribers;
+using System.Text.Json.Serialization;
+
+namespace NotificationCenterSdk.Topics;
+
+public class Topic
+{
+    [JsonPropertyName("_id")]
+    public string Id { get; set; }
+    
+    [JsonPropertyName("event")]
+    public string Event {get; set; }
+    
+    [JsonPropertyName("subject")]
+    public string SubjectId { get; set; }
+    
+    [JsonPropertyName("notificationTemplate")]
+    public string NotificationTemplateId { get; set; }
+
+    [JsonPropertyName("realm")]
+    public string realmId { get; set; }
+
+    [JsonPropertyName("parentTopic")]
+    public string ParentTopicId { get; set; }
+    
+    [JsonPropertyName("createdAt")]
+    public DateTime CreatedAt { get; set; }
+    
+    [JsonPropertyName("updatedAt")]
+    public DateTime UpdatedAt { get; set; }
+}
