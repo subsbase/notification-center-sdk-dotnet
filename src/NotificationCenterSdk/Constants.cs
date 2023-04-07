14d7cb93e132f249c770647d03282b4f065c99ce
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-04-07 15:11:27 +0200

    Constants added

---
+++ src/NotificationCenterSdk/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace NotificationCenterSdk;
+
+public static class Constants
+{
+    public const string TOPICS_PATH = "/topics";
+    public const string NOTIFICATIONS_PATH = "/notifications";
+    public const string SUBJECTS_PATH = "/subjects";
+    public const string SUBSCRIBERS_PATH = "/subscribers";
+}
