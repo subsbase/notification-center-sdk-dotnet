579fddb436437c0e9a5b1045543aaa1b39c17375
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-05-29 00:24:37 +0300

    update notification client

--- src/NotificationCenterSdk/Notifications/TriggerNotificationRequest.cs
+++ src/NotificationCenterSdk/Notifications/TriggerNotificationRequest.cs
@@ -7,9 +7,21 @@ public class TriggerNotificationRequest
     [JsonPropertyName("to")]
     public List<string> To { get; set; } = new();
     
-    [JsonPropertyName("payload")]
-    public object Payload { get; set; }
-    
     [JsonPropertyName("actionUrl")]
     public string ActionUrl { get; set; }
+    
+    
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    
+    [JsonPropertyName("message")]
+    public string Message { get; set; }
+    
+    
+    
+    [JsonPropertyName("templateId")]
+    public string TemplateId { get; set; }
+    
+    [JsonPropertyName("payload")]
+    public object Payload { get; set; }
 }
