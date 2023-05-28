e908917ae5b5bd16b06d8365d84934795d8dab74
Ibrahim Ahmed Badr <ibadr2122000@gmail.com>
2023-05-29 00:24:50 +0300

    update subject client

---
+++ src/NotificationCenterSdk/Subjects/UpdateSubjectRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NotificationCenterSdk.Subjects;
+
+public class UpdateSubjectRequest
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+}
